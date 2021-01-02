e3f50ae17d02f102f7d718e071b0e7fd8b69e293
Cominta <romanitstudents@gmail.com>
2021-01-02 15:57:51 +0200

    Исправление insert в array

--- array.py
+++ array.py
@@ -1,3 +1,5 @@
+import copy
+
 class Array():
 	def __init__(self, size):
 		self.arr = []
@@ -6,29 +8,48 @@ class Array():
 		for i in range(self.size):
 			self.arr.append(None)
 
+
+	#def __setattr__(self, attr, value):
+	#	if attr == "size":
+	#		raise AttributeError
+
+
 	def forEach(self, callback):
 		for i in range(len(self.arr)):
 			callback(self.arr[i])
+
 
 	def search(self, item):
 		for i in range(len(self.arr)):
 			if self.arr[i] == item:
 				return i
 
+
 	def insert(self, item, index):
-		if index > self.size:
-			raise IndexError(f"Index out of range")
+		oldArr = copy.copy(self.arr)
+		del self.arr
+		self.arr = []
+		self.size += 1
+
+		for i in range(self.size):
+			self.arr.append(None)
+
+		for i in range(0, index):
+			self.arr[i] = oldArr[i]
 
 		self.arr[index] = item
 
-arr = Array(5)
+		for i in range(index + 1, len(oldArr)):
+			self.arr[i] = oldArr[i]
+
+
+arr = Array(10)
 
 def printEach(item):
 	print(item)
 
-arr.insert(0, 0)
-arr.insert(1, 2)
-arr.insert(2, 1)
-arr.forEach(printEach)
-search = arr.search(0)
-print(search)
+print(arr.arr)
+arr.insert("y", 1)
+print(arr.arr)
+arr.insert("w", 5)
+print(arr.arr)
