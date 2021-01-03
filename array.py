8fcc7b420d10f86388bcbf890b154a90af5d5339
Cominta <romanitstudents@gmail.com>
2021-01-04 01:36:27 +0200

    Исправление insert и добавление тестов

--- array.py
+++ array.py
@@ -24,32 +24,136 @@ class Array():
 			if self.arr[i] == item:
 				return i
 
+		return -1
+
 
 	def insert(self, item, index):
+		if index > self.size and index < 0:
+			raise IndexError('Out of range')
+
 		oldArr = copy.copy(self.arr)
 		del self.arr
 		self.arr = []
 		self.size += 1
 
+		arrTemp = self.create_array(self.size)
+
 		for i in range(self.size):
-			self.arr.append(None)
+			if i == index:
+				j = i
+				for j in range(i, self.size):
+					arrTemp[j] = oldArr[j - 1]
 
-		for i in range(0, index):
-			self.arr[i] = oldArr[i]
+				arrTemp[i] = item
+				break
 
-		self.arr[index] = item
+			#print(arrTemp, oldArr)
+			arrTemp[i] = oldArr[i]
 
-		for i in range(index + 1, len(oldArr)):
-			self.arr[i] = oldArr[i]
+		self.arr = arrTemp
+		print(self.arr)
 
 
-arr = Array(10)
+	def delete(self, index):
+		if index > self.size and index < 0:
+			raise IndexError('Out of range')
 
-def printEach(item):
-	print(item)
+		oldArr = copy.copy(self.arr)
+		del self.arr
+		self.arr = []
+		self.size -= 1
 
-print(arr.arr)
-arr.insert("y", 1)
-print(arr.arr)
-arr.insert("w", 5)
-print(arr.arr)
+		arrTemp0 = oldArr[:index + 1]
+		arrTemp1 = oldArr[index + 1:]
+		arrTemp0.pop(index)
+		self.arr = arrTemp0 + arrTemp1 
+
+
+	def create_array(self, size):
+		arr = [None] * size
+		return arr
+
+
+def filling(arr):
+	for i in range(6):
+		arr.arr[i] = i + 1
+
+
+def test_array_insert():
+	arr = Array(6)
+
+	filling(arr)
+
+	arr.insert("a", 6)
+	assert arr.arr[6] == "a", "arr.arr[6] should be equal 'a', Insert to the end"
+	assert arr.size == 7, "arr.size should be equal 7"
+	for i in range(arr.size):
+		assert arr.arr[i] is not None, f"arr.arr[{i}] should not be equal None"
+
+	arr.insert("b", 5)
+	assert arr.arr[5] == "b", "arr.arr[6] should be equal 'b', Insert in the middle" 
+	assert arr.size == 8, "arr.size should be equal 8"
+	for i in range(arr.size):
+		assert arr.arr[i] is not None, f"arr.arr[{i}] should not be equal None"
+
+	arr.insert("c", 0)
+	assert arr.arr[0] == "c", "arr.arr[6] should be equal 'c', Insert to the start"
+	assert arr.size == 9, "arr.size should be equal 9" 
+	for i in range(arr.size):
+		assert arr.arr[i] is not None, f"arr.arr[{i}] should not be equal None"
+	#arr.insert("d", 15)
+
+	print("Test (insert) completed")
+
+
+def test_array_delete():
+	arr = Array(6)
+
+	filling(arr) 
+	# 1, 2, 3, 4, 5, 6
+
+	arr.delete(5)
+	# 1, 2, 3, 4, 5
+	assert arr.size == 5, "arr.size should be equal 5, Delete last element" 
+	for i in range(arr.size):
+		assert arr.arr[i] == i + 1, f"arr.arr[{i}] should be equal {i + 1}"
+
+	arr.delete(0)
+	# 2, 3, 4, 5
+	assert arr.size == 4, "arr.size should be equal 4, Delete first element"
+	for i in range(arr.size):
+		assert arr.arr[i] == i + 2, f"arr.arr[{i + 1}] should be equal {i + 1}" 
+
+	arr.delete(1)
+	# 2, 4, 5
+	assert arr.size == 3, "arr.size should be equal 3, Delete middle element"
+	assert arr.arr[0] == 2, "arr.arr[0] should be equal 2"
+	assert arr.arr[1] == 4, "arr.arr[1] should be equal 4"
+	assert arr.arr[2] == 5, "arr.arr[2] should be equal 5"
+
+	print("Test (delete) completed") 
+
+
+def test_array_search():
+	arr = Array(6)
+
+	arr.arr[0] = "a"
+	arr.arr[1] = "b"
+	arr.arr[2] = "c"
+	arr.arr[3] = "d"
+	arr.arr[4] = "f"
+	arr.arr[5] = "g"
+
+	index = arr.search("b")
+	assert index == 1, "index should be equal 1"
+	assert arr.arr[1] == "b", "arr.arr should be equal 'b'"
+
+	index = arr.search("r")
+	assert index == -1, "index should be equal -1, item not found"
+
+	print("Test (search) completed")
+
+
+test_array_insert()
+test_array_delete()
+test_array_search()
